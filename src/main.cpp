5e4a1f40675f86c1b989880802f50a979ad543b0
SamuelEBG <95290084+SamuelEBG@users.noreply.github.com>
2023-02-01 11:06:36 +0100

    working

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <MQTT.h>
 #include <WiFiMulti.h>
 #include <HTTPClient.h>
+#include <bits/stdc++.h>
 
 #include <Arduino.h>
 #include <ArduinoJson.h>
@@ -39,6 +40,7 @@ WiFiClient networkClient;
 MQTTClient mqttClient;
 
 unsigned long lastMillis = 0;
+unsigned long lastMillisTwo = 0;
 
 void connect() {
     Serial.print("checking wifi...");
@@ -103,146 +105,162 @@ String getRandomCity(const String cities[], const int sizeOfArray) {
     return cities[randomIndex];
 }
 
+  String tempAsString;
+  float temp;
+  float feelsLike;
+  String weatherMain;
+  String weatherDesc;
+  String country;
+  String city;
+  String locationFromApi;
+
 void loop() {
-    mqttClient.loop();
-    delay(10);  // <- fixes some issues with WiFi stability
-
-    if (!mqttClient.connected()) {
-        connect();
-    }
-
     // Weather API
     String location = getRandomCity(cities, amountOfCities);
     String endpoint = "http://api.openweathermap.org/data/2.5/weather?q=" + location + ",no&APPID=";
     const String key = "0fbfad07ebd8ba42e4384e55c8a77c58";
     const String units = "&units=metric";
 
+    if(millis() - lastMillisTwo > 10000){
+    lastMillisTwo = millis();
+    
     HTTPClient http;
-    HTTPClient httpPost;
 
     http.useHTTP10(true);
-    httpPost.useHTTP10(true);
 
-    // configure traged server and url
     http.begin(endpoint + key + units); //HTTP, GET weather from API
-    //httpPost.begin("http://dweet.io" + path);
 
-    // start connection and send HTTP header
     int httpCode = http.GET();
 
-    // httpCode will be negative on error
     if(httpCode > 0) {
-        // HTTP header has been send and Server response header has been handled
-        USE_SERIAL.println("");
-        USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
+      // HTTP header has been send and Server response header has been handled
+      USE_SERIAL.println("");
+      USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
 
-        // file found at server
-        if(httpCode == HTTP_CODE_OK) {
-            // String payload = http.getString();
+      // file found at server
+      if(httpCode == HTTP_CODE_OK) {
+          // String payload = http.getString();
 
-            DynamicJsonDocument doc(1024);
-            //StaticJsonDocument<200> doc;
-            // StaticJsonDocument<N> allocates memory on the stack, it can be
-            // replaced by DynamicJsonDocument which allocates in the heap.
-            //
-            // DynamicJsonDocument doc(200);
+          DynamicJsonDocument doc(1024);
+          //StaticJsonDocument<200> doc;
+          // StaticJsonDocument<N> allocates memory on the stack, it can be
+          // replaced by DynamicJsonDocument which allocates in the heap.
+          //
+          // DynamicJsonDocument doc(200);
 
-            // Deserialize the JSON document using getStream instead of getString
-            DeserializationError error = deserializeJson(doc, http.getStream());
+          // Deserialize the JSON document using getStream instead of getString
+          DeserializationError error = deserializeJson(doc, http.getStream());
 
-            // Test if parsing succeeds.
-            if (error) {
-                Serial.print(F("deserializeJson() failed: "));
-                Serial.println(error.f_str());
-                return;
-            }
+          // Test if parsing succeeds.
+          if (error) {
+              Serial.print(F("deserializeJson() failed: "));
+              Serial.println(error.f_str());
+              return;
+          }
 
-            // Fetch values.
-            //
-            // Most of the time, you can rely on the implicit casts.
-            // In other case, you can do doc["time"].as<long>();
-            //const char* name = doc["name"];
-            float temp = doc["main"]["temp"];
-            float feelsLike = doc["main"]["feels_like"];
-            String weatherMain = doc["weather"][0]["main"];
-            String weatherDesc = doc["weather"][0]["description"];
-            String country = doc["sys"]["country"];
-            String city = doc["name"];
-            String location = city + ", " + country;
+          // Fetch values.
+          //
+          // Most of the time, you can rely on the implicit casts.
+          // In other case, you can do doc["time"].as<long>();
+          //const char* name = doc["name"];
+          String tempString = doc["main"]["temp"];
+          tempAsString = tempString;
+          temp = doc["main"]["temp"];
+          feelsLike = doc["main"]["feels_like"];
+          String weatherM = doc["weather"][0]["main"];
+          weatherMain = weatherM;
+          String weatherD = doc["weather"][0]["description"];
+          weatherDesc = weatherD;
+          String c = doc["sys"]["country"];
+          country = c;
+          String ci = doc["name"];
+          city = ci;
+          locationFromApi = city + ", " + country;
 
-            // Change led according to the weather
-            if (weatherMain.equalsIgnoreCase("clouds"))
-            {
-                strip.setPixelColor(0, 0,0,255);
-                strip.show(); // Update strip with new contents
-            }
-            
-            if (weatherMain.equalsIgnoreCase("sunny"))
-            {
-                strip.setPixelColor(0, 255,255,0);
-                strip.show(); 
-            }
+          // Change led according to the weather
+          if (weatherMain.equalsIgnoreCase("clouds"))
+          {
+              strip.setPixelColor(0, 0,0,255);
+              strip.show(); // Update strip with new contents
+          }
+          
+          if (weatherMain.equalsIgnoreCase("sunny"))
+          {
+              strip.setPixelColor(0, 255,255,0);
+              strip.show(); 
+          }
 
-            if (weatherMain.equalsIgnoreCase("snow"))
-            {
-                strip.setPixelColor(0, 255,255,255);
-                strip.show();
-            }
+          if (weatherMain.equalsIgnoreCase("snow"))
+          {
+              strip.setPixelColor(0, 255,255,255);
+              strip.show();
+          }
 
-            if (weatherMain.equalsIgnoreCase("rain"))
-            {
-                strip.setPixelColor(0, 0, 255, 0);
-                strip.show();
-            }
-            
-            // Prepare payload to post to dweet
-            // "{\"api_key\":\"tPmAT5Ab3j7F9\",\"sensor\":\"BME280\",\"value1\":\"24.25\",\"value2\":\"49.54\",\"value3\":\"1005.14\"}"
-            /*
-            String postData = "{\"temperature\":\""; 
-            postData += weatherMain;
-            postData += "\",\"location\":\"";
-            postData += location;
-            postData += "\"}";
-            httpPost.addHeader("Content-Type", contentType);
-            
-           
-            // send the POST request
-            // read the status code and body of the response
-            int statusCode = httpPost.POST(postData);
+          if (weatherMain.equalsIgnoreCase("rain"))
+          {
+              strip.setPixelColor(0, 0, 255, 0);
+              strip.show();
+          }
+          
+          // Prepare payload to post to dweet
+          // "{\"api_key\":\"tPmAT5Ab3j7F9\",\"sensor\":\"BME280\",\"value1\":\"24.25\",\"value2\":\"49.54\",\"value3\":\"1005.14\"}"
+          /*
+          String postData = "{\"temperature\":\""; 
+          postData += weatherMain;
+          postData += "\",\"location\":\"";
+          postData += location;
+          postData += "\"}";
+          httpPost.addHeader("Content-Type", contentType);
+          
+          
+          // send the POST request
+          // read the status code and body of the response
+          int statusCode = httpPost.POST(postData);
 
-            Serial.print("Post status code: ");
-            Serial.println(statusCode);
-            */
-            
-            // Print values from GET to the terminal
-            /*
-            Serial.println(location);
-            Serial.println(weatherMain);
-            Serial.println(weatherDesc);
-            Serial.printf("Temperature: ");
-            Serial.println(temp);
-            Serial.printf("Feels like: ");
-            Serial.println(feelsLike);
-            */
+          Serial.print("Post status code: ");
+          Serial.println(statusCode);
+          */
+          
+          // Print values from GET to the terminal
+          /*
+          Serial.println(location);
+          Serial.println(weatherMain);
+          Serial.println(weatherDesc);
+          Serial.printf("Temperature: ");
+          Serial.println(temp);
+          Serial.printf("Feels like: ");
+          Serial.println(feelsLike);
+          */
+          
+          //mqttClient.publish(yourPersonalTopic.c_str(), "Temperate is " + tempAsString + " degrees and " + weatherDesc + " in " + location);
 
-            if (millis() - lastMillis > 5000) {
-                lastMillis = millis();
-                mqttClient.publish(yourPersonalTopic.c_str(), weatherDesc);
-            }
-        }
+          /*
+          if (millis() - lastMillis > 5000) {
+              lastMillis = millis();
+              mqttClient.publish(yourPersonalTopic.c_str(), "Temperate is" + tempAsString + " degrees and " + weatherDesc + " in " + location);
+          }
+          */
+      }
     } else {
         USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
     }
-    // httpPost.end();
     http.end();
+    }
 
+    mqttClient.loop();
+    delay(10);  // <- fixes some issues with WiFi stability
+
+    if (!mqttClient.connected()) {
+        USE_SERIAL.printf("connecting again \n");
+        connect();
+    }
+    
     // publish a message every 5 second.
-    /*
+    
     if (millis() - lastMillis > 5000) {
       lastMillis = millis();
-      mqttClient.publish(yourPersonalTopic.c_str(), "hello");
+      USE_SERIAL.printf("publishing\n");
+      mqttClient.publish(yourPersonalTopic.c_str(), "Temperate is " + tempAsString + " degrees and " + weatherDesc + " in " + locationFromApi);
     }
-    */
-       delay(15000);
 
 }
